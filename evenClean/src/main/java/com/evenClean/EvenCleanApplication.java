73042cf91b28b747e88f6118633dc8f39bf109f7
ravaeusz <girardirafael2003@gmail.com>
2025-12-17 12:53:16 -0300

    first commit

---
+++ evenClean/src/main/java/com/evenClean/EvenCleanApplication.java
@@ -0,0 +1,13 @@
+package com.evenClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenCleanApplication.class, args);
+	}
+
+}
---
+++ evenClean/src/test/java/com/evenClean/EvenCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
